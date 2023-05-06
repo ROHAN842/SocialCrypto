d5a84683b35d2da3c1df1f933e247992e6e1c6fc
ROHAN MODI <rohanmodi790@gmail.com>
2023-05-06 16:59:52 +0530

    Add files via upload

---
+++ SocialCrypto-Crowdfunding/client/src/constants/index.js
@@ -0,0 +1,20 @@
+import { createCampaign, dashboard, logout, payment, profile, withdraw } from '../assets';
+
+export const navlinks = [
+  {
+    name: 'dashboard',
+    imgUrl: dashboard,
+    link: '/',
+  },
+  {
+    name: 'campaign',
+    imgUrl: createCampaign,
+    link: '/create-campaign',
+  },
+  {
+    name: 'profile',
+    imgUrl: profile,
+    link: '/profile',
+  },
+ 
+];
